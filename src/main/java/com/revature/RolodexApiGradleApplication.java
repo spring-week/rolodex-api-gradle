846f7d5a2aea1821fd42944961352ffd26c4c306
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-08-19 13:59:28 -0400

    initial commit

---
+++ src/main/java/com/revature/RolodexApiGradleApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolodexApiGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolodexApiGradleApplication.class, args);
+	}
+
+}
