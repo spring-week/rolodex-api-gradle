5ae176674cf2dbb892aac24e137e4527f345aba4
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-08-21 13:04:41 -0400

    specify origin to resolve security vulnerability in CORS filter

--- src/main/java/com/revature/aspect/CorsFilter.java
+++ src/main/java/com/revature/aspect/CorsFilter.java
@@ -26,7 +26,7 @@ public class CorsFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain chain)
 			throws ServletException, IOException {
 
-		resp.setHeader("Access-Control-Allow-Origin", "*");
+		resp.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 		resp.setHeader("Access-Control-Allow-Credentials", "true");
 		resp.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH");
 		resp.setHeader("Access-Control-Allow-Headers",
